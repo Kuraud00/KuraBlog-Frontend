ce1066824c26e76f109ad37751482d64c053a0ea
Kuraudo <2756345706@qq.com>
2025-07-09 21:34:50 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="font-wdxl p-4 bg-blue-500 text-white rounded-lg gap-8 py-6">
+    <i class="fa-solid fa-house"></i>
+    如果此框有蓝色背景+白字+圆角，则Tailwind工作正常
+  </div>
+
+  <el-button>Default</el-button>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

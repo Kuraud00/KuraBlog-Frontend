bcd6275f0049b400e0db2adff505270aa96c3215
Kuraudo <2756345706@qq.com>
2025-07-11 21:02:54 +0800

    feat: modified sideNavigator

--- src/components/SideNavigator.vue
+++ src/components/SideNavigator.vue
@@ -3,26 +3,41 @@
     <!-- 侧边栏区域 -->
     <div
       class="font-bold text-2xl text-slate-50 h-screen hadow-xl bg-slate-600 py-2 select-none flex-col flex transition-all"
-      :class="{ 'w-40': isNavigatorOpen, 'w-4': !isNavigatorOpen }"
+      :class="{ 'w-56': isNavigatorOpen, 'w-16': !isNavigatorOpen }"
     >
       <span
-        class="h-20 text-center rounded-lg transition-colors overflow-hidden text-4xl content-center font-wdxl mb-8"
+        class="h-20 text-center rounded-lg transition-colors overflow-hidden text-4xl content-center font-wdxl mb-6"
         v-if="isNavigatorOpen"
         to="/home"
       >
         KuraBlog
       </span>
+
+      <div class="bg-slate-400 w-full h-0.5 mb-4" v-if="isNavigatorOpen"></div>
+
       <RouterLink
         v-for="item in menuItems"
-        class="text-center hover:bg-slate-500 rounded-lg transition-colors mx-2 font-roboto text-xl mb-4"
+        class="text-center hover:bg-slate-500 rounded-lg transition-colors mx-2 font-roboto text-xl mb-4 py-3"
         :to="item.direction"
-        v-if="isNavigatorOpen"
       >
         <i :class="item.icon"></i>
-        {{ item.description }}
+        <span class="pl-4" v-if="isNavigatorOpen">{{ item.description }}</span>
       </RouterLink>
+
+      <div class="mt-auto text-center">
+        <i
+          class="fa-solid fa-down-left-and-up-right-to-center transition-colors hover:bg-slate-500 p-4 rounded-lg"
+          @click="toggleNavigator"
+          v-if="isNavigatorOpen"
+        ></i>
+        <i
+          class="fa-solid fa-maximize transition-colors hover:bg-slate-500 p-4 rounded-lg"
+          @click="toggleNavigator"
+          v-else-if="!isNavigatorOpen"
+        ></i>
+      </div>
     </div>
-    <!-- 开关区域 -->
+    <!-- 开关区域
     <aside
       class="h-32 w-4 bg-slate-600 rounded-e-2xl transition-all hover:bg-slate-500 flex items-center"
       @click="toggleNavigator()"
@@ -31,7 +46,7 @@
         class="fa-solid text-white"
         :class="{ 'fa-chevron-right': !isNavigatorOpen, 'fa-chevron-left': isNavigatorOpen }"
       ></i>
-    </aside>
+    </aside> -->
   </aside>
 </template>
 
@@ -47,5 +62,7 @@ const toggleNavigator = () => {
 const menuItems: menuItem[] = [
   { icon: "fa-solid fa-house", description: "Home", direction: "/home" },
   { icon: "fa-solid fa-address-card", description: "About", direction: "/about" },
+  { icon: "fa-solid fa-blog", description: "Blog", direction: "/blog" },
+  { icon: "fa-solid fa-sheet-plastic", description: "Project", direction: "/project" },
 ];
 </script>

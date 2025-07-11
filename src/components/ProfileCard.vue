03231e7dfbd40f3b1f24d059305aba5e83ec2359
Kuraudo <2756345706@qq.com>
2025-07-11 21:18:41 +0800

    feat: finish profileCard

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,13 +1,36 @@
 <template>
   <div
-    class="w-52 h-[300px] rounded-xl bg-white shadow-round text-center pt-4 flex flex-col space-y-4 items-center"
+    class="w-60 h-[500px] rounded-xl bg-white shadow-round text-center pt-8 flex flex-col space-y-6 items-center"
   >
-    <div class="w-20 h-20 bg-black rounded-full"></div>
-    <div></div>
-    <RouterLink></RouterLink>
+    <img class="w-40 h-40 rounded-full overflow-hidden shadow-round object-cover" :src="requestURL"></img>
+    <span class="font-wdxl font-bold text-3xl text-slate-600">Kuraud0</span>
+
+    <span class="font-roboto text-md text-slate-600">
+      <i class="fa-solid fa-graduation-cap"></i>
+      University of Shanghai <br>for Science and Technology
+    </span>
+
+    <span class="font-wdxl text-2xl text-slate-600">
+      Find me:
+    </span>
+
+    <span class="flex flex-row text-3xl space-x-6 text-slate-600 content-center">
+      <a href="https://github.com/Kuraud00">
+        <i class="fa-brands fa-github"></i>
+      </a>
+      <a href="https://space.bilibili.com/481177373">
+        <i class="fa-brands fa-bilibili"></i>
+      </a>
+      <a href="">
+        <i class="fa-brands fa-qq"></i>
+      </a>
+    </span>
   </div>
 </template>
 
 <script setup>
-import { RouterLink } from "vue-router";
+// QQ号配置项
+const qqValue = "2756345706";
+const requestURL = `https://q1.qlogo.cn/g?b=qq&nk=${qqValue}&s=640`
+
 </script>
